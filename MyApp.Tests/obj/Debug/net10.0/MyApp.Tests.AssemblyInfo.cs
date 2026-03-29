a4472b68c4438154f2f83a9df5e07bdc1a1fb246
spashadzijski-web <spashadzijski@gmail.com>
2026-03-29 01:14:15 +0000

    Add solution file

--- MyApp.Tests/obj/Debug/net10.0/MyApp.Tests.AssemblyInfo.cs
+++ MyApp.Tests/obj/Debug/net10.0/MyApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad5a2e60b833836055208b3268fa9ef342b4bd55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78edd4b40a51c6b19f8098b4ecde9057653c91be")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
